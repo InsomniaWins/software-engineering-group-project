d41be7eba4050f6519b6db590182b33c6f3bcf70
Anthonyle3 <162517138+Anthonyle3@users.noreply.github.com>
2024-05-03 14:35:52 -0500

    .26
    return unlocked items from current item index

--- autoload/item_manager.gd
+++ autoload/item_manager.gd
@@ -1,19 +1,16 @@
 extends Node
 
 enum Items {
-	
-	SWORD,
-	BOW
-	
+    SWORD,
+    BOW
 }
-
 
 var unlocked_items = [Items.SWORD, Items.BOW]
 
+func get_next_available_item(current_item: int) -> Items:
+    var next_index = (current_item + 1) % unlocked_items.size()
+    return unlocked_items[next_index]
 
-# TODO: return then next element of unlocked_items from
-#       current_item's index in the unlocked_items array
-func get_next_available_item(current_item:int) -> int:
-	
-	
-	return Items.SWORD
+var current_item_index = Items.SWORD
+var next_item = get_next_available_item(current_item_index)
+print("Next available item:", next_item)
